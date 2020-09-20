43e118a19ca9d1ff5f60d0de2b753bd0e43667d8
Senlima Sun <senlima0430@gmail.com>
2020-09-20 20:01:36 +0800

    Update umami

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,15 +1,22 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="zh-TW">
-        <Head />
+        <Head>
+          <script
+            async
+            defer
+            data-website-id="3d450f27-000c-4d61-aa92-a111945e22f4"
+            src="https://umami-gilt.vercel.app/umami.js"
+          ></script>
+        </Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    );
+    )
   }
 }
